aef386a00c047dd6c94a5deceb573d18533572b0
Adobe Pham <xuanhau031297@gmail.com>
2024-09-15 10:52:21 +0700

    add top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Noto_Sans as FontSans } from 'next/font/google';
 import Head from 'next/head';
+import NextTopLoader from 'nextjs-toploader';
 
 import { ErrorBoundary } from '@/components/error-boundary';
 import { SEO_TAG } from '@/constants/seo';
@@ -36,6 +37,21 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
       <body className={cn('min-h-screen font-sans antialiased', fontSans.variable)} suppressHydrationWarning>
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
         <AppProvider>
           <ErrorBoundary>{children}</ErrorBoundary>
         </AppProvider>
